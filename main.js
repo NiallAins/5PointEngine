bc1bc7c743fa9e713d132cb17dfd309834f4f6ab
Niall Ainsworth <niallainsworth@gmail.com>
2015-08-06 17:22:07 +0100

    Update 06/08/15
    - Further attempts at correction projection formula
    - Formula correct for curving on single axis, but not combined x and y

--- main.js
+++ main.js
@@ -11,64 +11,42 @@ window.onload = function() {
     can5.width = can5.height = window.innerWidth * 0.4;
     ctx5.translate(can5.width / 2, can5.height / 2);
 
-    ctx0.strokeStyle = '#A22';
-    ctx5.strokeStyle = '#22A';
-    ctx0.lineWidth = 2;
-    ctx5.lineWidth = 2;
+    ctx0.lineWidth = 1;
+    ctx5.lineWidth = 1;
 
-    var scale = 1;
-    var box = [];
+    var scale = 5;
+    var box = Box(scale);
+    var boxTrans = [];
 
-    for(var i = 0; i < 20; i++) {
-        if (i % 2 === 0) {
-            for (var j = 0; j < 21; j++) {
-                box.push({ x : (i * (can0.width / 20)) - (can0.width / 2), y : (j * (can0.width / 20)) - (can0.width / 2) });
-            }
-        }
-        else {
-            for (var j = 20; j >= 0; j--) {
-                box.push({ x : (i * (can0.width / 20)) - (can0.width / 2), y : (j * (can0.width / 20)) - (can0.width / 2) });
-            }
-        }
+    for(var i = 0; i < box.length; i++) {
+        boxTrans.push({ x : box[i].x - (can0.width  / 2) ,
+                        y : box[i].y - (can0.height / 2)});
     }
-    for(var i = 0; i < 20; i++) {
-        if (i % 2 === 0) {
-            for (var j = 0; j < 21; j++) {
-                box.push({ x : (j * (can0.width / 20)) - (can0.width / 2), y : (i * (can0.width / 20)) - (can0.width / 2) });
-            }
-        }
-        else {
-            for (var j = 20; j >= 0; j--) {
-                box.push({ x : (j * (can0.width / 20)) - (can0.width / 2), y : (i * (can0.width / 20)) - (can0.width / 2) });
-            }
-        }
-    }
-
-    drawTo0(box);
-    drawTo5(box);
+    drawTo0(boxTrans);
+    drawTo5(boxTrans);
 
     function Box(scale) {
         var arr = []; 
-        for(var i = 0; i < 10*scale; i++) {
+        for(var i = 0; i < 10 * scale; i++) {
             if (i % 2 == 0) {
-                for(var j = 0; j < 10*scale; j++) {
+                for(var j = 0; j < 10 * scale; j++) {
                     arr.push({x : i * 10, y : j * 10});
                 }
             }
             else {
-                for(var j = (10*scale) - 1; j >= 0; j--) {
+                for(var j = (10 * scale) - 1; j >= 0; j--) {
                     arr.push({x : i * 10, y : j * 10});
                 }
             }
         }
-        for(var i = 0; i < 10*scale; i++) {
+        for(var i = 0; i < 10 * scale; i++) {
             if (i % 2 == 0) {
-                for(var j = (10*scale) - 1; j >= 0; j--) {
+                for(var j = (10 * scale) - 1; j >= 0; j--) {
                     arr.push({x : j * 10, y : i * 10});
                 }
             }
             else {
-                for(var j = 0; j < 10*scale; j++) {
+                for(var j = 0; j < 10 * scale; j++) {
                     arr.push({x : j * 10, y : i * 10});
                 }
             }
@@ -76,12 +54,12 @@ window.onload = function() {
         return arr;
     }
 
-    can0.addEventListener('mousemove', function(e) {
+    document.addEventListener('mousemove', function(e) {
         var boxTrans = [];
         var rect = can0.getBoundingClientRect();
         for(var i = 0; i < box.length; i++) {
             boxTrans.push({ x : box[i].x + e.clientX - (can0.width  / 2) - rect.left - (scale * 50),
-                         y : box[i].y + e.clientY - (can0.height / 2) - rect.top -  (scale * 50)});
+                            y : box[i].y + e.clientY - (can0.height / 2) - rect.top -  (scale * 50)});
         }
         drawTo0(boxTrans);
         drawTo5(boxTrans);
@@ -95,24 +73,32 @@ window.onload = function() {
         drawTo5(box);
     });
             
-    function to5pp(p) {
-        var r = can5.width / 2;
+    function to5pp(p, ax) {
+        var r = can5.width / 2,
+            scaleX = p.x / r,
+            scaleY = p.y / r;
 
-        var scaleX = p.x / r;
-        var scaleY = p.y / r;
+        var newX = scaleX * Math.sqrt(1 - (scaleY * scaleY)),
+            newY = scaleY * Math.sqrt(1 - (scaleX * scaleX));
 
-        var newX = p.x * Math.sqrt(1 - (scaleY * scaleY));
-        var newY = p.y * Math.sqrt(1 - (scaleX * scaleX));
+        newX *= r;
+        newY *= r;
 
-        return {x : newX, y: newY};
+        if (typeof(ax) === 'undefined')
+            return {x : newX, y: newY};
+        else if (ax === 'x')
+            return {x : newX, y: p.y};
+        else
+            return {x : p.x, y: newY};
     };
 
     function drawTo0(pts) {
         ctx0.clearRect(-can0.width / 2, -can0.height / 2, can0.width, can0.height);
                 
+        ctx0.strokeStyle = '#22F';
         ctx0.beginPath();
             ctx0.moveTo(pts[0].x, pts[0].y);
-            for(var i = 0; i < pts.length; i++) {
+            for(var i = 0; i < pts.length; i ++) {
                 ctx0.lineTo(pts[i].x, pts[i].y);
             }
         ctx0.stroke();
@@ -120,14 +106,34 @@ window.onload = function() {
 
     function drawTo5(pts) {
         ctx5.clearRect(-can5.width / 2, -can5.height / 2, can5.width, can5.height);
-                
+        
+        var pt = to5pp(pts[0], 'x');
+        ctx5.strokeStyle = '#F22';
         ctx5.beginPath();
-            var pt = to5pp(pts[0]);
             ctx5.moveTo(pt.x, pt.y);
-            for(var i = 0; i < pts.length; i++) {
-                pt = to5pp(pts[i]);
+            for(var i = 0; i < pts.length / 2; i ++) {
+                pt = to5pp(pts[i], 'x');
                 ctx5.lineTo(pt.x, pt.y);
             }
-        ctx5.stroke();   
+        ctx5.stroke();
+
+        var pt = to5pp(pts[pts.length / 2], 'y');
+        ctx5.strokeStyle = '#F22';
+        ctx5.beginPath();
+            ctx5.moveTo(pt.x, pt.y);
+            for(var i = pts.length / 2; i < pts.length; i ++) {
+                pt = to5pp(pts[i], 'y');
+                ctx5.lineTo(pt.x, pt.y);
+            }
+        ctx5.stroke();
+
+        /*for(var i = 0; i < pts.length; i++) {
+            pt = to5pp(pts[i]);
+            ctx5.fillStyle = '#2F2';
+            ctx5.fillRect(pt.x - 3, pt.y - 3, 6, 6);
+            pt = to5pp2(pts[i]);
+            ctx5.fillStyle = '#22F';
+            ctx5.fillRect(pt.x - 3, pt.y - 3, 6, 6);
+        }*/
     };
 };

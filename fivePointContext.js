71caffd494c5237086392eb518104cbab7e789c1
Niall Ainsworth <niallainsworth@gmail.com>
2015-08-08 12:51:33 +0100

    Update 08/08/15
    - Moved 5pp function to object
    - added line and rectangle to 5pp functions

---
+++ fivePointContext.js
@@ -0,0 +1,123 @@
+function FPCtx(canvas, context) {
+	var can = canvas,
+		ctx = context,
+		r = canvas.width / 2,
+	    prevP = {x : 0, y : 0};
+
+	var sq = function(num) {
+        return num * num;
+    }
+
+    var clean = function(p, r) {
+        if (typeof(r) === 'undefined') { r = can.width / 2; }
+
+        var cx = p.x / r;
+        var cy = p.y / r;
+
+        cx = Math.min(cx,  0.95);
+        cy = Math.min(cy,  0.95);
+        cx = Math.max(cx, -0.95);
+        cy = Math.max(cy, -0.95);
+
+        if (cx === 0) { cx = 0.0001 };
+        if (cy === 0) { cy = 0.0001 };
+
+        cx *= r;
+        cy *= r;
+
+        return {x : cx, y : cy};
+    }
+
+    //Params:  Array - three points as [x0, y0, x1, y1, x2, y2]
+    //Returns: Array - circle center as [0,1] and radius as [2] 
+    var to5p = function(p) {
+        //Scale point between 1 and -1
+        var r = can.width / 2;
+        var scaleP = clean({x : p.x / r, y : p.y / r}, 1);
+        var scaleX = scaleP.x;
+        var scaleY = scaleP.y;
+
+        //Get each new axis by taking arc through point and vanishing points
+        var xAx = arcUsing([1, 0, -1, 0, 0, scaleY]);
+        var yAx = arcUsing([0, 1, 0, -1, scaleX, 0]);
+
+        //Get new point at new axis intersection
+        var newPt = arcIntersec(xAx, yAx);
+
+        //Rescale
+        newPt.x *= r;
+        newPt.y *= r;
+
+        return newPt;
+    }
+
+	//Params:  Array - three points as [x0, y0, x1, y1, x2, y2]
+    //Returns: Array - circle center as [0,1] and radius as [2]
+    var arcUsing = function(p) {
+        var k = 0.5 * (((sq(p[0])+sq(p[1])) * (p[4]-p[2])) + ((sq(p[2])+sq(p[3])) * (p[0]-p[4])) + ((sq(p[4])+sq(p[5])) * (p[2]-p[0]))) / (p[1] * (p[4]-p[2])+(p[3] * (p[0]-p[4])+(p[5] * (p[2]-p[0]))));  
+        var h = 0.5 * (((sq(p[0])+sq(p[1])) * (p[5]-p[3])) + ((sq(p[2])+sq(p[3])) * (p[1]-p[5])) + ((sq(p[4])+sq(p[5])) * (p[3]-p[1]))) / (p[0] * (p[5]-p[3])+(p[2] * (p[1]-p[5])+(p[4] * (p[3]-p[1]))));
+        var r = Math.sqrt(sq(p[0] - h) + sq(p[1] - k));
+        
+        return [h, k, r];
+    }
+
+    //Params:  Two Arrays - circle centers as [0,1] and radius' as [2]
+    //Returns: Object - intersection point of two circles
+    var arcIntersec = function(c0, c1) {
+        var p0 = [c0[0], c0[1], c0[2]];
+        var p1 = [c1[0], c1[1], c1[2]];
+
+        var d = Math.sqrt(sq(p0[1] - p1[1]) + sq(p0[0] - p1[0])); 
+        var a = (sq(p0[2]) - sq(p1[2]) + d*d)/(2*d);
+        var h = Math.sqrt(sq(p0[2]) - a*a);
+        var p2 = [((p1[0] - p0[0]) * (a/d)) + p0[0], ((p1[1] - p0[1]) * (a/d)) + p0[1]];
+        var x3 = p2[0] + h*(p1[1] - p0[1])/d;
+        var y3 = p2[1] - h*(p1[0] - p0[0])/d;
+        var x4 = p2[0] - h*(p1[1] - p0[1])/d;
+        var y4 = p2[1] + h*(p1[0] - p0[0])/d;
+
+        //Return relavent intersection point
+        if (x3 * y3 > 0)
+            return { x : x3, y : y3 };
+        else
+            return { x : x4, y : y4 };
+    }
+
+    //Params:  Array - three points as [x0, y0, x1, y1, x2, y2]
+    //Returns: Array - circle center as [0,1] and radius as [2] 
+    this.line = function(p00, p10) {
+        p00 = clean(p00);
+        p10 = clean(p10);
+
+        var yi = p00.y - ((p10.y - p00.y) / (p10.x - p00.x) * p00.x);
+        if (yi ===  Infinity) { yi = -r; }
+        if (yi === -Infinity) { yi =  r; }
+
+        var p05 = to5p(p00);
+        var p15 = to5p(p10);
+
+        var c = arcUsing([p05.x, p05.y, 0, yi, p15.x, p15.y]);
+
+        var sAng = Math.atan2(p05.y - c[1], p05.x - c[0]) + (Math.PI * 2);
+        var eAng = Math.atan2(p15.y - c[1], p15.x - c[0]) + (Math.PI * 2);
+
+        var arcAng = eAng - sAng;
+        if (arcAng >  Math.PI) { arcAng -= 2 * Math.PI; }
+        if (arcAng < -Math.PI) { arcAng += 2 * Math.PI; }
+
+        ctx.beginPath();
+        	ctx.arc(c[0], c[1], c[2], sAng, eAng, arcAng < 0);
+        ctx.stroke();
+    }
+
+    this.rect = function(cP, w, h) {
+    	var p0 = {x:cP.x - w, y:cP.y - h},
+    	    p1 = {x:cP.x - w, y:cP.y + h},
+    	    p2 = {x:cP.x + w, y:cP.y + h},
+    	    p3 = {x:cP.x + w, y:cP.y - h};
+    	this.line(p0, p1);
+    	this.line(p1, p2);
+    	this.line(p2, p3);
+    	this.line(p3, p0);
+    }
+}
--- main.js
+++ main.js
@@ -10,11 +10,14 @@ window.onload = function() {
         ctx5 = can5.getContext('2d');
     can5.width = can5.height = window.innerWidth * 0.4;
     ctx5.translate(can5.width / 2, can5.height / 2);
+    var fpCtx = new FPCtx(can5, ctx5);
 
     ctx0.lineWidth = 1;
     ctx5.lineWidth = 1;
 
-    var scale = 5;
+    var mouseP = {x:0, y:0};
+
+    var scale = 2;
     var box = Box(scale);
     var boxTrans = [];
 
@@ -58,8 +61,8 @@ window.onload = function() {
         var boxTrans = [];
         var rect = can0.getBoundingClientRect();
         for(var i = 0; i < box.length; i++) {
-            boxTrans.push({ x : box[i].x + e.clientX - (can0.width  / 2) - rect.left - (scale * 50),
-                            y : box[i].y + e.clientY - (can0.height / 2) - rect.top -  (scale * 50)});
+            boxTrans.push({ x : box[i].x + e.clientX - (can0.width  / 2) - rect.left - (scale *50),
+                            y : box[i].y + e.clientY - (can0.height / 2) - rect.top  - (scale * 50)});
         }
         drawTo0(boxTrans);
         drawTo5(boxTrans);
@@ -73,93 +76,24 @@ window.onload = function() {
         drawTo5(box);
     });
             
-    //Params:  Array - three points as [x0, y0, x1, y1, x2, y2]
-    //Returns: Array - circle center as [0,1] and radius as [2] 
-    function to5pp(p) {
-        //Scale point between 1 and -1
-        var r = can5.width / 2,
-            scaleX = p.x / r,
-            scaleY = p.y / r;
-
-        //Round extreme values
-        scaleX = Math.min(scaleX,  0.95);
-        scaleY = Math.min(scaleY,  0.95);
-        scaleX = Math.max(scaleX, -0.95);
-        scaleY = Math.max(scaleY, -0.95);
-
-        //Get each new axis by taking arc through point and vanishing points
-        var xAx = arcUsing([1, 0, -1, 0, 0, scaleY]);
-        var yAx = arcUsing([0, 1, 0, -1, scaleX, 0]);
-
-        //Get new point at new axis intersection
-        var newPt = arcIntersec(xAx, yAx);
-
-        //Rescale
-        newPt.x *= r;
-        newPt.y *= r;
-
-        return newPt;
-    };
-
     function drawTo0(pts) {
         ctx0.clearRect(-can0.width / 2, -can0.height / 2, can0.width, can0.height);
                 
         ctx0.strokeStyle = '#22F';
+        ctx0.moveTo(pts[0].x, pts[0].y);
         ctx0.beginPath();
-            ctx0.moveTo(pts[0].x, pts[0].y);
-            for(var i = 0; i < pts.length; i ++) {
-                ctx0.lineTo(pts[i].x, pts[i].y);
-            }
+        for(var i = 0; i < pts.length - 1; i++) {
+            ctx0.lineTo(pts[i].x, pts[i].y);
+        }
         ctx0.stroke();
     };
 
     function drawTo5(pts) {
         ctx5.clearRect(-can5.width / 2, -can5.height / 2, can5.width, can5.height);
         
-        var pt = to5pp(pts[0]);
         ctx5.strokeStyle = '#F22';
-        ctx5.beginPath();
-            ctx5.moveTo(pt.x, pt.y);
-            for(var i = 0; i < pts.length; i ++) {
-                pt = to5pp(pts[i]);
-                ctx5.lineTo(pt.x, pt.y);
-            }
-        ctx5.stroke();
+        for(var i = 0; i < pts.length - 1; i++) {
+            fpCtx.line(pts[i], pts[i + 1]);
+        }
     };
-
-    //Params:  Array - three points as [x0, y0, x1, y1, x2, y2]
-    //Returns: Array - circle center as [0,1] and radius as [2]
-    function arcUsing(p) {
-        var k = 0.5 * (((sq(p[0])+sq(p[1])) * (p[4]-p[2])) + ((sq(p[2])+sq(p[3])) * (p[0]-p[4])) + ((sq(p[4])+sq(p[5])) * (p[2]-p[0]))) / (p[1] * (p[4]-p[2])+(p[3] * (p[0]-p[4])+(p[5] * (p[2]-p[0]))));  
-        var h = 0.5 * (((sq(p[0])+sq(p[1])) * (p[5]-p[3])) + ((sq(p[2])+sq(p[3])) * (p[1]-p[5])) + ((sq(p[4])+sq(p[5])) * (p[3]-p[1]))) / (p[0] * (p[5]-p[3])+(p[2] * (p[1]-p[5])+(p[4] * (p[3]-p[1]))));
-        var r = sq(p[0] - h) + sq(p[1] - k);
-        
-        return [h, k, r];
-    }
-
-    //Params:  Two Arrays - circle centers as [0,1] and radius' as [2]
-    //Returns: Object - intersection point of two circles
-    function arcIntersec(c0, c1) {
-        var p0 = [c0[0], c0[1], Math.sqrt(c0[2])];
-        var p1 = [c1[0], c1[1], Math.sqrt(c1[2])];
-
-        var d = Math.sqrt(sq(p0[1] - p1[1]) + sq(p0[0] - p1[0])); 
-        var a = (sq(p0[2]) - sq(p1[2]) + d*d)/(2*d);
-        var h = Math.sqrt(sq(p0[2]) - a*a);
-        var p2 = [((p1[0] - p0[0]) * (a/d)) + p0[0], ((p1[1] - p0[1]) * (a/d)) + p0[1]];
-        var x3 = p2[0] + h*(p1[1] - p0[1])/d;
-        var y3 = p2[1] - h*(p1[0] - p0[0])/d;
-        var x4 = p2[0] - h*(p1[1] - p0[1])/d;
-        var y4 = p2[1] + h*(p1[0] - p0[0])/d;
-
-        //Return relavent intersection point
-        if (x3 * y3 > 0)
-            return { x : x3, y : y3 };
-        else
-            return { x : x4, y : y4 };
-    }
-
-    function sq(num) {
-        return num * num;
-    }
 };
